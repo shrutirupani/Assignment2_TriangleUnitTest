99c90a7243e3ca3819a528ad30c26f5cdadde458
Shruti Rupani <srupani8595@conestogac.on.ca>
2019-06-04 13:39:58 -0400

    Add project files.

---
+++ Assignment2_8040/TriangleSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_8040
+{
+   public static class TriangleSolver
+    {
+        public static string Analyze( int side1 , int side2 , int side3)
+        {
+            if(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+            {
+                Console.WriteLine("It is Triangle");
+                
+                 if( side1 == side2 && side2 == side3)
+                {
+                    return ("It is Equilateral Triangle");
+                }
+                else if (side1 == side2 || side1 == side3 || side2 == side3)
+                {
+                    return ("It is Isosceles Triangle");
+                }
+                else
+                {
+                    return ("It is Scalene Triangle");
+                } 
+            }
+            else
+            {
+                return ("It is not a Triangle");
+            } 
+        }
+    }
+}
